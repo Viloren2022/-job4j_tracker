6cd90df52947b30188f7f9f429f345cf4206e5cd
Viloren88 <garifullin.marat@gmail.com>
2022-03-06 00:35:52 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,32 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    Error() {
+    }
+
+    Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Статус ошикби " + active);
+        System.out.println("Приоритет " + status);
+        System.out.println("Устранить за " + message);
+    }
+
+    public static void main(String[] args) {
+        Error fatal = new Error(true, 10, "за 30 минут");
+        fatal.printInfo();
+        Error critikal = new Error();
+        critikal.printInfo();
+        Error priority = new Error();
+        priority.printInfo();
+
+    }
+}
