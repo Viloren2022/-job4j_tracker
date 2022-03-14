a9a8ddee4d940f6463f8a1b1abb844fd6ef91731
Viloren2022 <marat.garifullin88@gmail.com>
2022-03-14 23:12:15 +0300

    3.2. Зачем нужно приведение типов [#310019]

---
+++ src/main/java/ru/job4j/oop/Bus.java
@@ -0,0 +1,15 @@
+package ru.job4j.oop;
+
+public class Bus implements Vehicle {
+    @Override
+    public void move() {
+        System.out.println("Выполняется городской маршрут");
+    }
+
+    @Override
+    public int fill(int fuel) {
+        int countFuel = 0;
+        countFuel += fuel;
+        return countFuel;
+    }
+}
---
+++ src/main/java/ru/job4j/oop/Train.java
@@ -0,0 +1,15 @@
+package ru.job4j.oop;
+
+public class Train implements Vehicle {
+    @Override
+    public void move() {
+        System.out.println("Выполняется междугородний рейс");
+    }
+
+    @Override
+    public int fill(int fuel) {
+        int countFuel = 0;
+        countFuel += (fuel * 2);
+        return countFuel;
+    }
+}
