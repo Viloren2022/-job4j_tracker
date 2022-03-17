21234e99b8c7bf8107565615e4cb9d497843f128
Viloren2022 <marat.garifullin88@gmail.com>
2022-03-17 22:24:30 +0300

    9.2. Тесты вывода на консоль [#33585]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -138,7 +138,7 @@ public class StartUITest {
     }
 
     @Test
-    public void whenFindByIdTestOutputIsSuccessfully() {
+    public void whenFindByIDTestOutputIsSuccessfully() {
         Output out = new StubOutput();
         Tracker tracker = new Tracker();
         Item one = tracker.add(new Item("test1"));
