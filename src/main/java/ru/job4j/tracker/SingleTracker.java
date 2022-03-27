9f2b019b30d4217dcdd3cd7970fada5d9eb624ad
Viloren2022 <marat.garifullin88@gmail.com>
2022-03-27 23:10:06 +0300

    1. Singleton [#94606]

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,41 @@
+package ru.job4j.tracker;
+
+public class SingleTracker {
+    private Tracker tracker = new Tracker();
+    private static SingleTracker checkCopy = null;
+
+    private SingleTracker() {
+    }
+
+    public static SingleTracker getCheckCopy() {
+        if (checkCopy == null) {
+            checkCopy = new SingleTracker();
+        }
+        return checkCopy;
+    }
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public Item findById(int id) {
+        return tracker.findById(id);
+    }
+
+    public Item[] findByName(String key) {
+        return tracker.findByName(key);
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+
+    }
+
+    public boolean delete(int id) {
+        return tracker.delete(id);
+    }
+}
