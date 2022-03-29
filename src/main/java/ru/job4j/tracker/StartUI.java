3b51c74de6deb7a6fa422d467d78224d42c4971c
Viloren88 <garifullin.marat@gmail.com>
2022-03-29 21:26:16 +0300

    2.1. Тест на ValidateInput [#311887 #279560]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -30,7 +30,8 @@ public class StartUI {
 
     public static void main(String[] args) {
         Output output = new ConsoleOutput();
-        Input input = new ValidateInput();
+        Input input = new ConsoleInput();
+        Input input2 = new ValidateInput(output, input);
         Tracker tracker = new Tracker();
         UserAction[] actions = {
                 new CreateAction(output),
