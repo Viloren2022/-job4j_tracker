97d2c8a1305115d104f3680eba67d975f2bd0942
Viloren88 <garifullin.marat@gmail.com>
2022-04-10 22:43:25 +0300

    6. Изменить программу Tracker из 2-го модуля [#10039]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -22,6 +22,7 @@ public class Tracker {
         for (int index = 0; index < items.size(); index++) {
             if (items.get(index).getId() == id) {
                 rsl = index;
+                break;
             }
         }
         return rsl;
