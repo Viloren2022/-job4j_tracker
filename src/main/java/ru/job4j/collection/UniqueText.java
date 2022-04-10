6f5d85dc7dd7dd913ec80282d59fd82dd666a4fc
Viloren88 <garifullin.marat@gmail.com>
2022-04-10 22:41:36 +0300

    2. Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,18 @@
+package ru.job4j.collection;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>(Arrays.asList(origin));
+        for (String t : text) {
+            rsl = check.contains(t);
+            break;
+        }
+        return rsl;
+    }
+}
