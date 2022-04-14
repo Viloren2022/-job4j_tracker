bf843061bce3a7b2df9327a336078b833c62659b
Viloren88 <garifullin.marat@gmail.com>
2022-04-14 20:57:46 +0300

    1. Паспорт и жители [#212656 #287264]

--- src/main/java/ru/job4j/collection/PassportOffice.java
+++ src/main/java/ru/job4j/collection/PassportOffice.java
@@ -17,6 +17,6 @@ public class PassportOffice {
     }
 
     public Citizen get(String passport) {
-        return citizens.getOrDefault(passport, null);
+        return citizens.get(passport);
     }
 }
