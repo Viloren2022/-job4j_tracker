ce4712c03bd3c6634d8231c2610315965e0e5d85
Viloren88 <garifullin.marat@gmail.com>
2022-04-29 00:02:51 +0300

    1. Фильтрация учеников [#110058]

---
+++ src/main/java/ru/job4j/stream/School.java
@@ -0,0 +1,24 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class School {
+    public static List<Student> collect(List<Student> students, Predicate<Student> predict) {
+        return students.stream()
+                .filter(predict)
+                .collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        List<Student> students2 = List.of(
+                new Student(10, "Surname1"),
+                new Student(40, "Surname4"),
+                new Student(50, "Surname5"),
+                new Student(70, "Surname7"),
+                new Student(90, "Surname9")
+        );
+        //collect(students2,predict.negate();
+    }
+}
